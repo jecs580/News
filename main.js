8d841906d227e8cfaa795d42ff42c9414ed77abb
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-04-22 20:14:58 -0400

    Agregando ScrollReveal para animacion de elementos

--- main.js
+++ main.js
@@ -7,8 +7,15 @@ si tiene el simbolo "#" antes de su de la palabra para saber que es por id, caso
 css.
 */
 document.querySelector('.menu-btn').addEventListener('click',()=> {
-    document.querySelector('.nav-menu').classList.toggle('show');
+    document.querySelector('.nav-menu').classList.toggle('show');});
     /*Con el metodo classList accedemos a lista de clases y con el metodo .toggle() declaramos una 
     funcion que permite alternar para agregar o quitar una clase especifica.*/ 
-    
-});
+
+    // Libreria SCROLLREVEAL
+    // Agregando animacion al momento de cargar la pagina
+    ScrollReveal().reveal('.showcase');  
+    // Agregamos la misma animacion pero con una duracion de 0.5 segundos.
+    ScrollReveal().reveal('.news-cards',{delay:500}); 
+    ScrollReveal().reveal('.cards-banner-one',{delay:500});
+    ScrollReveal().reveal('.cards-banner-two',{delay:500});
+
